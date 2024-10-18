f83498aae697e0bd85e804dde4951ffdebc2b715
Rajlodh-001 <Rajlodh911@gmail.com>
2024-10-18 21:12:03 +0530

    javascript multiple dice roller

---
+++ JS_P4_MultipleDiceRoller/index.js
@@ -0,0 +1,28 @@
+function rollDice(){
+
+
+    const numofDice = document.getElementById("numofDice").value;
+
+    const diceResult =document.getElementById("diceResult");
+    const diceImages =document.getElementById("diceImages");
+
+    const values = [];
+    const images=[];
+
+
+    for (let i =0 ; i<numofDice;i++){
+        const value =Math.floor(Math.random()*6)+1;
+
+        values.push(value)
+        // console.log(value)
+        // images.push(`dice value ${value}`)
+        
+
+        // for images 
+        images.push(`<img src="dice_images/${value}.png"> alt="Dice : ${value}">`)
+    }
+    diceResult.textContent=`dice : ${values.join(`, `)}`
+    diceImages.innerHTML=images.join(' ')
+
+
+}
